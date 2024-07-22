02dd3c01db1ea659f47f72345d90cfe264d9be7d
Darko Golovic <golovicdarko@gmail.com>
2024-07-22 11:39:54 +0200

    cjenovnik page

--- frontend/src/pages/Cjenovnik.jsx
+++ frontend/src/pages/Cjenovnik.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import "../styles/cjenovnik.css";
 
 export const Cjenovnik = () => {
-    return <div>Cjenovnik</div>;
+    const services = [
+        { id: 1, name: "Konsultacija", price: "50€" },
+        { id: 2, name: "Osnovni pregled", price: "30€" },
+        { id: 3, name: "Napredni pregled", price: "70€" },
+        { id: 4, name: "Laboratorijske analize", price: "40€" },
+        { id: 5, name: "Specijalistički pregled", price: "100€" },
+    ];
+    return (
+        <main className="cjenovnik-holder">
+            <div className="cjenovnik-title">
+                <h1>MNE-CLINICS Cjenovnik</h1>
+            </div>
+
+            <div className="cjenovnik-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Usluga</th>
+                            <th>Cijena</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {services.map((service) => (
+                            <tr key={service.id}>
+                                <td>{service.name}</td>
+                                <td>{service.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </main>
+    );
 };
